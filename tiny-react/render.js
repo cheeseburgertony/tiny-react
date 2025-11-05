913ea765b023d06678e3c163b15051e8557111c3
cheeseburgertony <1821955020@qq.com>
2025-11-05 20:09:29 +0800

    feat: 替换递归渲染为调度函数(Concurrent Mode)以优化渲染性能

--- tiny-react/render.js
+++ tiny-react/render.js
@@ -12,11 +12,31 @@ function render(element, container) {
       dom[key] = element.props[key];
     });
 
-  // 递归渲染子元素
-  element.props.children.forEach((child) => render(child, dom));
-
   // 将创建的DOM元素添加到容器（父元素）中
   container.appendChild(dom);
 }
+
+let nextUnitOfWork = null;
+
+// 工作循环 调度函数（替换之前的递归渲染）
+function workLoop(deadline) {
+  // 是否停止
+  let shouldYield = false;
+
+  // 还有任务并且不应该退出的时候
+  while (nextUnitOfWork && !shouldYield) {
+    // 执行单元工作并且拿到下一个工作单元
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    // 判断在这一帧中是否还有足够的时间
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  // 没有足够的时间则退出循环，并且请求下一次浏览器空闲的时候执行
+  requestIdleCallback(workLoop);
+}
+
+// 第一次请求
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(nextUnitOfWork) {}
 
 export default render;
