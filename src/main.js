0cb73a6d21b854c69627bef29254e2339da6d42d
cheeseburgertony <1821955020@qq.com>
2025-11-05 17:17:50 +0800

    feat: 实现render函数

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createElement } from "../tiny-react";
+import { createElement, render } from "../tiny-react";
 
 const element = createElement(
   "h1",
-  { id: "title", className: "hello" },
+  { id: "title", className: "hello", style: "color: skyblue;" },
   "Hello, Tiny React!",
-  createElement("div", null, "This is a div inside h1")
+  createElement("div", { style: "color: pink;" }, "This is a div inside h1")
 );
+
+const root = document.querySelector("#root");
+render(element, root);
 
 console.log(element);
